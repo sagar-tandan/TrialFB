38f147d8c8621a59bb514891a4445c2145f5b94c
Sagar Tandan <076bct072@ioepc.edu.np>
2024-10-24 13:44:44 +0545

    hero section

--- src/USER/Header.jsx
+++ src/USER/Header.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [activetab, setActiveTab] = useState("home");
   return (
     <div className="w-full h-[60px] flex bg-[#1E1E1E] text-white px-14 items-center justify-center">
       <div className="w-full max-w-screen-2xl mx-auto flex justify-between">
@@ -9,10 +10,10 @@ const Header = () => {
           <img src="" alt="" />
           <span>LOGO</span>
         </div>
-        <div className="flex gap-14 py-2 font-semibold">
-          <span className="cursor-pointer hover:text-purple-600">Home</span>
-          <span className="cursor-pointer hover:text-purple-600">Products</span>
-          <span className="cursor-pointer hover:text-purple-600">About Us</span>
+        <div className="flex gap-14 font-semibold py-2">
+          <span className={`cursor-pointer`}>Home</span>
+          <span className="cursor-pointer">Products</span>
+          <span className="cursor-pointer">About Us</span>
         </div>
 
         <div className="bg-[#141414] px-6 py-2 rounded-md border-[1px] border-gray-800 font-semibold cursor-pointer hover:bg-purple-600 transition-all duration-200 ease-in-out">
